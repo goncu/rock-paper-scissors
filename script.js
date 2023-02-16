238de27fa6c2d57f69c68b42f512bedf5ac5dafa
goncu <116492383+goncu@users.noreply.github.com>
2023-02-16 21:52:30 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,18 @@
-const introScreen = document.querySelector(`.intro-screen`);
-const gameScreen = document.querySelector(`.game-screen`);
-const startButton = document.querySelector(`.btn-start`);
-const roundResult = document.querySelector(`.round-result`);
-const roundResultText = document.querySelector(`.round-result-text`);
-const finalResult = document.querySelector(`.final-result`);
-const endScreen = document.querySelector(`.end-screen`);
-const playAgain = document.querySelector(`.btn-again`);
-const playerScoreel = document.querySelector(`.player-score`);
-const computerScoreel = document.querySelector(`.computer-score`);
-let playerScore = 0;
-let computerScore = 0;
-let playerChoice, computerChoice;
-let roundPlayed = false; // This line is in order to make buttons non-functional after a round is played.
+// function for selecting CSS class
+const selectClass = function (el) {
+  return document.querySelector(el);
+};
 
-// Below code block is for the function of start button and initializing the game.
-startButton.addEventListener(`click`, () => {
-  introScreen.classList.add(`hidden`);
-  gameScreen.classList.remove(`hidden`);
-});
+// function for the text declaring who won the round.
+const declaringRoundResult = function () {
+  return `You ${roundWinner === 0 ? `won` : `lost`}! You chose ${
+    playerChoice === 0 ? `rock` : playerChoice === 1 ? `paper` : `scissors`
+  }${roundWinner === 0 ? ` and` : `, but`} the computer chose ${
+    computerChoice === 0 ? `rock` : computerChoice === 1 ? `paper` : `scissors`
+  }.`;
+};
 
-//below function is for ending the round, checking whether winning condition is achived end preparing for new round.
+//Function for ending the round, checking whether winning condition is achived end preparing for new round.
 const endRound = function () {
   setTimeout(() => {
     if (playerScore === 5) {
@@ -39,55 +32,56 @@ const endRound = function () {
   }, 2500);
 };
 
+// variables for DOM manipulation
+const introScreen = selectClass(`.intro-screen`),
+  gameScreen = selectClass(`.game-screen`),
+  startButton = selectClass(`.btn-start`),
+  roundResult = selectClass(`.round-result`),
+  roundResultText = selectClass(`.round-result-text`),
+  finalResult = selectClass(`.final-result`),
+  endScreen = selectClass(`.end-screen`),
+  playAgain = selectClass(`.btn-again`),
+  playerScoreel = selectClass(`.player-score`),
+  computerScoreel = selectClass(`.computer-score`);
+let playerScore = 0,
+  computerScore = 0,
+  playerChoice,
+  computerChoice,
+  roundPlayed = false, // This line is in order to make buttons non-functional after a round is played.
+  roundWinner;
+
+// Below code block is for the function of start button and initializing the game.
+startButton.addEventListener(`click`, () => {
+  introScreen.classList.add(`hidden`);
+  gameScreen.classList.remove(`hidden`);
+});
+
 // Below code block is for game logic. For loop is for applying it to all 3 buttons.
 // 0 = rock, 1 = paper, 2 = scissors
 for (let i = 0; i < 3; i++) {
-  document.querySelector(`.btn-${i}`).addEventListener(`click`, function () {
+  selectClass(`.btn-${i}`).addEventListener(`click`, function () {
     if (!roundPlayed) {
       roundResult.classList.remove(`hidden`);
       roundPlayed = true;
       playerChoice = i;
       computerChoice = Math.trunc(Math.random() * 3);
-      // Below if conditional is for deciding who wins the round.
       if (
         (playerChoice === 0 && computerChoice === 2) ||
         (playerChoice === 1 && computerChoice === 0) ||
         (playerChoice === 2 && computerChoice === 1)
       ) {
+        roundWinner = 0;
         playerScore++;
-        roundResultText.textContent = `You won! You chose ${
-          playerChoice === 0
-            ? `rock`
-            : playerChoice === 1
-            ? `paper`
-            : `scissors`
-        } and the computer chose ${
-          computerChoice === 0
-            ? `rock`
-            : computerChoice === 1
-            ? `paper`
-            : `scissors`
-        }.`;
+        roundResultText.textContent = declaringRoundResult();
         endRound();
       } else if (
         (computerChoice === 0 && playerChoice === 2) ||
         (computerChoice === 1 && playerChoice === 0) ||
         (computerChoice === 2 && playerChoice === 1)
       ) {
+        roundWinner = 1;
         computerScore++;
-        roundResultText.textContent = `You lost! You chose ${
-          playerChoice === 0
-            ? `rock`
-            : playerChoice === 1
-            ? `paper`
-            : `scissors`
-        }, but the computer chose ${
-          computerChoice === 0
-            ? `rock`
-            : computerChoice === 1
-            ? `paper`
-            : `scissors`
-        }.`;
+        roundResultText.textContent = declaringRoundResult();
         endRound();
       } else if (playerChoice === computerChoice) {
         roundResultText.textContent = `It's a draw! Choose again.`;
@@ -104,8 +98,8 @@ playAgain.addEventListener(`click`, function () {
   playerScore = 0;
   computerScore = 0;
   roundPlayed = false;
-  document.querySelector(`.player-score`).textContent = 0;
-  document.querySelector(`.computer-score`).textContent = 0;
+  selectClass(`.player-score`).textContent = 0;
+  selectClass(`.computer-score`).textContent = 0;
   endScreen.classList.add(`hidden`);
   gameScreen.classList.remove(`hidden`);
 });
